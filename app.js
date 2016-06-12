28abca74ee45ffc03ac4d6ce04827326e1987771
Cher Stewart <cmstewart@blizzard.com>
2016-06-11 20:56:35 -0700

    fix deploy error?

--- app.js
+++ app.js
@@ -10,28 +10,28 @@ var express = require('express'),
     passport = require('passport'),
     GitHubStrategy = require('passport-github').Strategy;
  
-passport.use(new GitHubStrategy({
-    clientID: "c7280bf6b04f562c0fb3",
-    clientSecret: "c2a8b35a613ab49d9433760e75a7cfa57fb05cc4",
-    callbackURL: "/auth/github/callback"
-  },
-  function(accessToken, refreshToken, profile, cb) {
-    User.findOrCreate({ githubId: profile.id }, function (err, user) {
-      return cb(err, user);
-    });
-  }
-));
+// passport.use(new GitHubStrategy({
+//     clientID: "c7280bf6b04f562c0fb3",
+//     clientSecret: "c2a8b35a613ab49d9433760e75a7cfa57fb05cc4",
+//     callbackURL: "/auth/github/callback"
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     User.findOrCreate({ githubId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
 
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
+// passport.serializeUser(function(user, cb) {
+//   cb(null, user);
+// });
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
+// passport.deserializeUser(function(obj, cb) {
+//   cb(null, obj);
+// });
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use('/public', serveStatic(__dirname + '/public'));
 
